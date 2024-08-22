7772c43b39f8680d4a8251f5163d18d6182605c3
chengwei.fu <fuchengwei0127@gmail.com>
2024-08-22 17:32:42 +0800

    feat: add server response model

---
+++ src/model/server-response.model.ts
@@ -0,0 +1,11 @@
+export class ServerResponse<T> {
+  status: number
+  message: string
+  data?: T
+
+  constructor(status: number, message: string, data?: T) {
+    this.status = status
+    this.message = message
+    this.data = data
+  }
+}
