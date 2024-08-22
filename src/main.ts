965ff57c593974a720aabb6a839496ec5019e7ea
chengwei.fu <fuchengwei0127@gmail.com>
2024-08-22 17:56:29 +0800

    feat: add validation pipe for global use

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { HttpExceptionFilter } from '@/common/filter/http-exception.filter'
 import { ResponseInterceptor } from '@/common/interceptor/response.interceptor'
@@ -7,6 +8,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } }))
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new ResponseInterceptor())
 
